3498ddf896bbefce1161113e552cb7a398246c84
nirjhar sarkar <nirjharsarkar8@gmail.com>
2023-12-14 18:50:03 +0530

    added messages to the socket server and added some custom styling into the css file

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,7 +4,12 @@ class SocketService {
       private _io: Server;
       constructor() {
             console.log("SocketService");
-            this._io = new Server();
+            this._io = new Server({
+                  cors:{
+                        allowedHeaders:["*"],
+                        origin:"*",
+                  }
+            });
       }
 
       public initListeners() {
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import React, { useCallback, useEffect } from 'react'
-import { io } from 'socket.io-client'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
+import { io, Socket } from 'socket.io-client'
 
 interface ISocketContext {
       // eslint-disable-next-line no-unused-vars
@@ -12,19 +12,31 @@ interface ISocketContext {
 
 const SocketContext = React.createContext<ISocketContext | null>(null)
 
+export const useSocket = () => {
+      const state = useContext(SocketContext)
+      if (!state) { throw new Error('state is undefined') };
+      return state
+}
+
 const SocketProvider: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+      const [socket, setSocket] = useState<Socket>()
 
       useEffect(() => {
             const _socket = io('http://localhost:8000');
+            setSocket(_socket)
             return () => {
                   _socket.disconnect();
+                  setSocket(undefined)
             }
       }, [])
 
 
       const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
             console.log("send message", msg)
-      }, [])
+            if (socket) {
+                  socket.emit('event:message', { message: msg });
+            }
+      }, [socket])
 
 
       return (
